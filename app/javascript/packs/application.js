bb121b8c50086c66c8e84a42afa29cdca83c0de9
Herve Lao <laoherve@outlook.com>
2019-11-19 16:48:30 +0100

    navbar footer banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+
+import { on } from '../plugins/init_overlay';
+import { off } from '../plugins/init_overlay';
+
+on();
+off();
---
+++ app/javascript/plugins/init_overlay.js
@@ -0,0 +1,10 @@
+const on = () => {
+  document.getElementById("overlay").style.display = "block";
+}
+
+const off = () => {
+  document.getElementById("overlay").style.display = "none";
+}
+
+export { on };
+export { off };
