24f6213acc17ca3d842d52d4a4595591d831f191
Javier Franco <javier.franco18@gmail.com>
2019-11-20 18:35:48 +0100

    Get images for index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_144847) do
+ActiveRecord::Schema.define(version: 2019_11_20_133326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_11_18_144847) do
     t.string "continent"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.text "description"
   end
 
   create_table "places", force: :cascade do |t|
