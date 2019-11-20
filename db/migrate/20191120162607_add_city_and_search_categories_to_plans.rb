dee93ae4552148f1d65edd4019be7f99b7c765ed
Niklas Wagener <n-wagener@web.de>
2019-11-20 17:00:20 +0100

    added migration

--- db/migrate/20191120162607_add_city_and_search_categories_to_plans.rb
+++ db/migrate/20191120162607_add_city_and_search_categories_to_plans.rb
@@ -1,6 +1,6 @@
 class AddCityAndSearchCategoriesToPlans < ActiveRecord::Migration[5.2]
   def change
     add_column :plans, :city_id, :integer
-    add_column :plans, :search_items, :array, default: []
+    add_column :plans, :search_items, :string, array: true, default: []
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_150415) do
+ActiveRecord::Schema.define(version: 2019_11_20_162607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2019_11_20_150415) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.string "search_items", default: [], array: true
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
