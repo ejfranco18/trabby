1e4bf9f06b5a7d9e988b5b1cba0b51ecdd3fbc5c
Niklas Wagener <n-wagener@web.de>
2019-11-22 13:19:07 +0100

    changed activity generator and date query in plan creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_175162) do
+ActiveRecord::Schema.define(version: 2019_11_22_105053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_175162) do
     t.string "name"
     t.string "address"
     t.string "images"
-    t.text "description"
+    t.string "description", default: [], array: true
     t.string "opening_hours"
     t.float "latitude"
     t.float "longitude"
@@ -83,22 +83,12 @@ ActiveRecord::Schema.define(version: 2019_11_21_175162) do
     t.index ["user_id"], name: "index_preferences_on_user_id"
   end
 
-<<<<<<< HEAD
-  create_table "prices", force: :cascade do |t|
-    t.integer "value"
-    t.date "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "request_caches", force: :cascade do |t|
     t.string "url", null: false
     t.json "response", default: {}
     t.index ["url"], name: "index_request_caches_on_url", unique: true
   end
 
-=======
->>>>>>> master
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,12 @@ User.destroy_all
 Activity.destroy_all
 Plan.destroy_all
 City.destroy_all
-Price.destroy_all
 Category.destroy_all
 Place.destroy_all
 
 # user1 = User.create(first_name: "Charles", last_name: "Owns", email: "charles@gmail.com", password: "123456", image: "https://unsplash.com/photos/WBGjg0DsO_g")
 # plan1 = Plan.create(start_date: "25.12.2019", end_date: "02.01.2020", user_id:8)
 # city1 = City.create!(name:"Madrid", country:"Spain", continent: "Europe")
-# price1 = Price.create!(value: 20, date:"10.11.2019")
 # # category1 = Category.create!(category: "Museum")
 # place1 = Place.create!(name: "Great Place", address: "somehwere in Madrid", images:"https://unsplash.com/photos/WBGjg0DsO_g", description:"amazing to spend your day", opening_hours: "10 am", latitude: 40.416775, longitude: -3.703790, duration: 3, link:"", type: "", city_id: 1, price_id: 1, category_id: 1)
 # activity1 = Activity.create!(start_time:"10.30", end_time:"12.30", place_id:1, plan_id:1)
