35c42e2e39386d44e3d0cc52094c7fbdacd7b769
Javier Franco <javier.franco18@gmail.com>
2019-11-26 11:05:35 +0100

    Fix js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '../plugins/user_category';
 import { initMapbox } from '../plugins/init_mapbox';
-// import { flkty } from '../plugins/flickity';
-// import 'flickity/dist/flickity.css';
-import homeScroll from '../plugins/scrollmagic'
+import '../plugins/scrollmagic'
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import "../plugins/category_choice";
 import "../plugins/flatpickr"
@@ -15,16 +13,16 @@ import { initSortableAddAnEvent } from '../plugins/init_sortable';
 import { TxtType } from '../plugins/typewriterEffect';
 
 
-if (document.body.id.match('pages_home')) {
-  homeScroll();
-}
+// if (document.body.id.match('pages_home')) {
+//   homeScroll();
+// }
 
 
-const showModal = () => {
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
-}
+// const showModal = () => {
+//   $('#myModal').on('shown.bs.modal', function () {
+//     $('#myInput').trigger('focus')
+//   })
+// }
 
 initAutocomplete();
 initMapbox();
@@ -38,7 +36,7 @@ animForm.addEventListener("submit", (event) => {
   load();
   setTimeout((event) => {
     animForm.submit();
-  }, 2000)
+  }, 3000)
 })
 
 // const animLink = document.querySelector(".animTrigger");
