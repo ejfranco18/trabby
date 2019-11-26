c5d9ec9e1b5da56bdd3b5cccf00579810500cffd
Niklas Wagener <n-wagener@web.de>
2019-11-26 17:41:57 +0100

    changed style of location homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :resource
+  def resource
+    @resource = User.new
+  end
+
 end
