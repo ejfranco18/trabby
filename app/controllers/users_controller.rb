0d9df2b2f710305ca61c5410c4bd516bc07e8ac7
Javier Franco <javier.franco18@gmail.com>
2019-11-28 14:46:32 +0100

    Fix profile redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to user_path(@user)
     end
   end
 
